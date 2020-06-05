2721a0fe09c96b408aca27546bf82e2e3ba9fe4c
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-05 22:40:42 +0000

    Automatically backed up by Learn

--- lib/your_solution.rb
+++ lib/your_solution.rb
@@ -1,3 +1,4 @@
 # Write your solution here
-expect(STDOUT). to recieve(puts).with('Hello World!')
+expect(STDOUT). to recieve
+(puts)
 # This file should output "Hello World!" using a puts statement
